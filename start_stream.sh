5bcf7e5d9930fc941a35e52d53c6cfc68ae1f2d3
VJ Pranay <pranaymadasi1@gmail.com>
2021-10-25 19:59:44 +0530

    Update start_stream.sh

--- start_stream.sh
+++ start_stream.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Script to launch a live rtmp streaming session
 # The script populates .env file with environment variables passed by docker-compose.yml
+cd owncast && ./owncast &
+
+cd /root/bbb-recorder
 
 echo rtmpUrl=$RTMP_URL > .env
 echo ffmpegServer=$FFMPEG_SERVER >> .env
@@ -12,6 +15,8 @@ echo playbackFile=$PLAYBACK_FILE >> .env
 echo "debug env variables in .env"
 cat .env
 echo $JOIN_MEETING_URL
+
+
 
 node ffmpegServer.js &
 
